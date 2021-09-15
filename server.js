95fa7d467b1cf6a5e0a3d52030837e95af8a568e
Robin F <rfussell17@gmail.com>
2021-09-14 17:15:16 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get('/api/customers',(req, res) => {
+    const customers = [
+        {id: 1, firstName: 'John', lastName: 'Doe'},
+        {id: 2, firstName: 'Kim', lastName: 'Rae'},
+        {id: 3, firstName: 'Ted', lastName: 'Mi'},
+    ];
+    
+    res.json(customers);
+})
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
